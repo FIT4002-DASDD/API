aa7d53ecc0485d7fddef7179e2458a16ea119e33
Uyen Tran <utra0001@student.monash.edu>
2021-09-13 17:30:03 +1000

    modify TwitterBot to store more bot info

--- src/models/TwitterBot.ts
+++ src/models/TwitterBot.ts
@@ -8,16 +8,57 @@ import {
 } from "typeorm";
 import { TwitterAdSeenByBot } from ".";
 
+enum TwitterBotType {
+  AMERICA = "america",
+  AUSTRALIA = "australia",
+  UNSPECIFIED = "unspecified",
+}
+
+enum TwitterPoliticalRanking {
+  LEFT = 0,
+  CENTRE_LEFT = 1,
+  CENTRE = 2,
+  CENTRE_RIGHT = 3,
+  RIGHT = 4,
+  UNSPECIFIED = 5,
+}
+
 @Entity()
 export class TwitterBot extends BaseEntity {
+  public static BOT_TYPE = TwitterBotType;
+  public static POLITICAL_RANKING = TwitterPoliticalRanking;
+
   @PrimaryColumn("varchar")
   id!: string; // Same as username, not removed for compatibility reason
 
   @Column("varchar")
   username!: string;
 
-  @Column("int")
-  politicalRanking!: number;
+  @Column("varchar", { nullable: true })
+  password!: string;
+
+  @Column({
+    type: "enum",
+    enum: TwitterBot.BOT_TYPE,
+    default: TwitterBot.BOT_TYPE.UNSPECIFIED,
+  })
+  type!: typeof TwitterBot.BOT_TYPE;
+
+  @Column({
+    type: "enum",
+    enum: TwitterBot.POLITICAL_RANKING,
+    default: TwitterBot.POLITICAL_RANKING.UNSPECIFIED,
+  })
+  politicalRanking!: typeof TwitterBot.POLITICAL_RANKING;
+
+  @Column("text", { array: true, default: [] })
+  followedAccounts!: string[];
+
+  @Column("text", { array: true, default: [] })
+  relevantTags!: string[];
+
+  @Column("date", { nullable: true })
+  dob!: Date;
 
   @OneToMany(() => TwitterAdSeenByBot, (adToTag) => adToTag.bot)
   adBot?: TwitterAdSeenByBot[];
